9bf91d0d55e7a3a394df911c4b203fb62bb54f20
lvsouza <ls18017@gmail.com>
2024-09-24 11:17:39 -0300

    refactor: improve dev mode and more

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,19 @@ import { defineConfig } from 'vite';
 
 
 export default defineConfig({
+  root: './',
   build: {
+    sourcemap: false,
+    emptyOutDir: true,
     lib: {
       formats: ['es'],
       entry: './src/extension.ts',
       name: 'export-project-extension',
       fileName: () => `index.js`,
     },
-    rollupOptions: {
-      external: [],
-    },
-    sourcemap: true,
-    emptyOutDir: true,
   },
   server: {
     port: 3000,
-    cors: {
-      origin: '*',
-    },
+    cors: { origin: '*' },
   }
 })
